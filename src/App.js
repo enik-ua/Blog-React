0fa605c1587bc558471f549ec21571e5a4911d78
enik-ua <enik@mail.ua>
2023-11-15 14:18:49 +0200

    Public,Facebook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const FB = Object.getOwnPropertyDescriptor(window,"FB");
+  const [login,setLogin]=useState();
+  let token;
+
+  FB.value.init({
+    appId      : '214407322793199',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v12.0'});
+
   return (
     <div className="App">
+      {!login && <button className="signInWithFacebook" onClick={signInWithFacebook} >Sign in with Facebook</button>}
       <header className="App-header">
+          
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,6 +34,21 @@ function App() {
       </header>
     </div>
   );
+  function signInWithFacebook() {
+    debugger
+    const me = this;
+    const FB = Object.getOwnPropertyDescriptor(window,"FB");
+    FB.value.login(
+      function(response){
+        if (response.authResponse) {
+          token = response.authResponse.accessToken;
+          setLogin(Number(response.authResponse.userID));
+        }else{
+
+        }          
+      },
+      {scope: 'public_profile,email'});
+  }
 }
 
 export default App;
